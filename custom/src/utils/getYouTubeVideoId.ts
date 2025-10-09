52e536ef4e307e7fe4174a7bf2d103b43f470f8f
lopezrunco <lopezrunco@gmail.com>
2025-10-09 01:04:46 -0300

    refactor: Update regexs in getYoutubeVideoId.ts.

--- custom/src/utils/getYouTubeVideoId.ts
+++ custom/src/utils/getYouTubeVideoId.ts
@@ -1,13 +1,14 @@
 // Takes a string type URL parameter & returns a video ID string or null.
 export const getYouTubeVideoId = (url: string): string | null => {
     const youtubeLinkPatterns = [
-        /https:\/\/www\.youtube\.com\/embed\/([^"?]+)/,  // Embed URL
-        /https:\/\/www\.youtube\.com\/watch\?v=([^"&?/]+)/,  // Long URL
-        /https:\/\/m\.youtube\.com\/watch\?v=([^"&?/]+)/,  // Mobile URL
-        /https:\/\/youtu\.be\/([^"&?/]+)/,  // Short URL
-        /https:\/\/www\.youtube\.com\/live\/([^"?]+)/,  // Live stream URL
-        /https:\/\/www\.youtube\.com\/watch\?v=([^"&?/]+)&t=\d+S/,  // Live stream URL with timestamp
-        /https:\/\/m\.youtube\.com\/live\/([^"?]+)/  // Mobile live stream URL
+        /(?:https?:\/\/)?(?:www\.)?youtube\.com\/embed\/([^"?]+)/,              // Embed URL
+        /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([^"&?/]+)/,          // Long URL
+        /(?:https?:\/\/)?(?:m\.)?youtube\.com\/watch\?v=([^"&?/]+)/,            // Mobile URL
+        /(?:https?:\/\/)?youtu\.be\/([^"&?/]+)/,                                // Short URL
+        /(?:https?:\/\/)?(?:www\.)?youtube\.com\/live\/([^"?]+)/,               // Live stream URL
+        /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([^"&?/]+)&t=\d+S/,   // Long URL with timestamp
+        /(?:https?:\/\/)?(?:m\.)?youtube\.com\/live\/([^"?]+)/,                 // Mobile live stream URL
+        /(?:https?:\/\/)?youtube\.com\/live\/([^"?]+)/                          // No subdomain live stream
     ]
 
     for (const pattern of youtubeLinkPatterns) {
