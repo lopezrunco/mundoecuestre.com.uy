c2e25e905f7f3042b5dbdf04d316f72a691a5f9b
lopezrunco <lopezrunco@gmail.com>
2025-11-12 08:31:58 -0300

    fix: Clean code.

--- custom/src/interfaces/post.ts
+++ custom/src/interfaces/post.ts
@@ -8,7 +8,6 @@ export interface Post {
     categories: number[];
     acf: {
         inicio_de_la_transmision: string;
-        fin_de_la_transmision: string;
         enlace_transmision?: string;
         enlace_a_preoferta?: string;
         ubicacion?: string;
